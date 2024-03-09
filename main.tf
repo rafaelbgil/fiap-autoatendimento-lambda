57eca78c13e17e6938f3ea6fb47718a3f74b790a
Rafael Benite Gil <rafaelbgil@gmail.com>
2024-03-09 14:28:29 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,32 @@
+variable "iam" {
+    type        = string
+    description = "Variavel utilizada para informar o iam"
+  }
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+data "archive_file" "criarUsuarioCognitoZip" {
+  type        = "zip"
+  source_file = "src/criarUsuarioCognito/lambda_function.py"
+  output_path = "criarUsuarioCognito.py.zip"
+}
+
+resource "aws_lambda_function" "criarUsuarioCognito" {
+  function_name    = "criarUsuario"
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  filename         = "criarUsuarioCognito.py.zip"
+  source_code_hash = data.archive_file.criarUsuarioCognitoZip.output_base64sha256
+  role             = var.iam
+}
+
+
---
+++ src/criarUsuarioCognito/lambda_function.py
@@ -0,0 +1,62 @@
+import boto3
+from botocore.config import Config
+
+def lambda_handler(event, context):
+    config = Config(region_name='us-east-1')
+    # client = boto3.client('cognito-idp', aws_access_key_id='sua_access_key',
+    #                      aws_secret_access_key='sua_secret_key', config=config)
+
+
+    client = boto3.client('cognito-idp', config=config)
+    attributos = [
+        {
+            'Name': 'name',
+            'Value': event['nome']
+        },
+        {
+            'Name': 'email',
+            'Value': event['email']
+        }
+        ,
+        {
+            'Name': 'email_verified',
+            'Value': 'true'
+        }
+    ]
+
+    if 'cpf' in event:
+        attributos.append(
+            {'Name': 'custom:cpf',
+             'Value': event['cpf']
+             }
+        )
+    try:
+        client.admin_create_user(
+            UserPoolId='us-east-1_aEdl4I2z8',
+            Username=event['email'],
+            UserAttributes=attributos,
+
+            ForceAliasCreation=False,
+            MessageAction='SUPPRESS',
+
+            ClientMetadata={
+                'string': 'string'
+            }
+        )
+
+        response = client.admin_set_user_password(
+            UserPoolId='us-east-1_aEdl4I2z8',
+            Username=event['email'],
+            Password=event['senha'],
+            Permanent=True
+        )
+    except:
+        return {
+            'statusCode': 400,
+            'body': 'error'
+        }
+    client.close()
+    return {
+        'statusCode': 201,
+        'body': response.__str__()
+    }
